4a1b018363bce9f87b596555a4adecacbe50d96b
afischer <andreas.fischer@cyber.bka.de>
2025-02-18 12:15:07 +0100

    init abstraction

---
+++ go.mod
@@ -0,0 +1,26 @@
+module phevaluator
+
+go 1.23
+
+require (
+	github.com/mattlangl/gophe v0.0.0-20240117021521-d65a1ba156dc
+	github.com/schollz/progressbar/v3 v3.18.0
+	gorm.io/driver/postgres v1.5.11
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/term v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
---
+++ main_test.go
@@ -0,0 +1,255 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_calculateHandStrength(t *testing.T) {
+	type args struct {
+		holeCards []string
+		board     []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want handStrength
+	}{
+		{
+			name: "pocket_asse",
+			args: args{holeCards: []string{"Ah", "As"}, board: []string{"2s", "3d", "Ks", "Qc", "7c"}},
+			want: handStrength{Wins: 884, Loss: 105, Draws: 1},
+		},
+		{
+			name: "royal_flush",
+			args: args{holeCards: []string{"Ah", "Kh"}, board: []string{"Jh", "Th", "Qh", "Qc", "7c"}},
+			want: handStrength{Wins: 990, Loss: 0, Draws: 0},
+		},
+		{
+			name: "royal_flush_turn",
+			args: args{holeCards: []string{"Ah", "Kh"}, board: []string{"Jh", "Th", "Qh", "Qc"}},
+			want: handStrength{Wins: 45540, Loss: 0, Draws: 0},
+		},
+		{
+			name: "pocket_asse_turn",
+			args: args{holeCards: []string{"Ah", "As"}, board: []string{"2s", "3d", "Ks", "Qc"}},
+			want: handStrength{Wins: 40062, Loss: 5434, Draws: 44},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := calculateHandStrength(tt.args.holeCards, tt.args.board); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("calculateHandStrength() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func Test_generateCombinations(t *testing.T) {
+	type args struct {
+		deck            []string
+		combinationSize int
+		start           int
+		current         []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]string
+	}{
+		{
+			name: "basic combination size 2",
+			args: args{deck: []string{"A", "B", "C", "D"}, combinationSize: 2, start: 0, current: []string{}},
+			want: [][]string{
+				{"A", "B"},
+				{"A", "C"},
+				{"A", "D"},
+				{"B", "C"},
+				{"B", "D"},
+				{"C", "D"},
+			},
+		},
+		{
+			name: "basic combination size 3",
+			args: args{deck: []string{"A", "B", "C", "D"}, combinationSize: 3, start: 0, current: []string{}},
+			want: [][]string{
+				{"A", "B", "C"},
+				{"A", "B", "D"},
+				{"A", "C", "D"},
+				{"B", "C", "D"},
+			},
+		},
+		{
+			name: "basic combination move start",
+			args: args{deck: []string{"A", "B", "C", "D"}, combinationSize: 2, start: 1, current: []string{}},
+			want: [][]string{
+				{"B", "C"},
+				{"B", "D"},
+				{"C", "D"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := generateCombinations(
+					tt.args.deck,
+					tt.args.combinationSize,
+					tt.args.start,
+					tt.args.current,
+				); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("generateCombinations() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func Test_generateAbstractedHoleCards(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{name: "generate hole cards", want: 169},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := len(generateAbstractedHoleCards()); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("generateAbstractedHoleCards() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func Test_generateAbstractedBoards(t *testing.T) {
+	type args struct {
+		holeCards           []string
+		countCommunityCards int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "abstract board",
+			args: args{holeCards: []string{"3c", "3d"}, countCommunityCards: 3},
+			want: 11124,
+		},
+		{
+			name: "abstract board suited hole cards",
+			args: args{holeCards: []string{"3d", "4d"}, countCommunityCards: 3},
+			want: 5035,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := len(
+					generateAbstractedBoards(
+						tt.args.holeCards,
+						tt.args.countCommunityCards,
+					),
+				); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("generateAbstractedBoards() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+func Test_generateHandCombinations(t *testing.T) {
+	type args struct {
+		countCommunityCards int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "flop hand combinations", args: args{countCommunityCards: 3}, want: 1374984},
+		{name: "turn hand combinations", args: args{countCommunityCards: 4}, want: 15633514},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := len(generateHandCombinations(tt.args.countCommunityCards)); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("generateHandCombinations() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+//func Test_abstractHand(t *testing.T) {
+//	type args struct {
+//		cards []string
+//	}
+//	tests := []struct {
+//		name string
+//		args args
+//		want []string
+//	}{
+//		{
+//			name: "basic abstraction",
+//			args: args{cards: []string{"2c", "2d", "2h", "3c", "3h"}},
+//			want: []string{"2c", "2d", "2h", "3c", "3"},
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(
+//			tt.name, func(t *testing.T) {
+//				if got := abstractHand(tt.args.cards); !reflect.DeepEqual(got, tt.want) {
+//					t.Errorf("abstractHand() = %v, want %v", got, tt.want)
+//				}
+//			},
+//		)
+//	}
+//}
+
+func Test_abstractHand(t *testing.T) {
+	type args struct {
+		holeCards      []string
+		communityCards []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "sort hole and community cards", args: args{
+				holeCards: []string{"2c", "2d"}, communityCards: []string{
+					"2h", "3h", "3c",
+				},
+			}, want: []string{"2c", "2d", "2h", "3c", "3h"},
+		},
+		{
+			name: "basic abstraction", args: args{
+				holeCards: []string{"2c", "2d"}, communityCards: []string{
+					"3c", "3d", "5s",
+				},
+			}, want: []string{"2c", "2d", "3c", "3d", "5h"},
+		},
+		{
+			name: "basic abstraction", args: args{
+				holeCards: []string{"2c", "2d"}, communityCards: []string{
+					"3c", "3d", "5h",
+				},
+			}, want: []string{"2c", "2d", "3c", "3d", "5h"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				if got := abstractHand(tt.args.holeCards, tt.args.communityCards); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("abstractHand() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
